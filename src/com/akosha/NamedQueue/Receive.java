ed5091bb58888fe97440e0f1cb69a46340436c0e
unknown <prathap.choudhury@gmail.com>
2015-01-04 01:49:22 +0530

    workqueue added

---
+++ src/com/akosha/NamedQueue/Receive.java
@@ -0,0 +1,33 @@
+package com.akosha.NamedQueue;
+
+import java.io.IOException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+
+public class Receive {
+	private final static String QUEUE_NAME = "hello";
+	
+	public static void main(String args[]) throws IOException, InterruptedException {
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+		
+		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		System.out.println("[*] waiting for messages. To exit press CTRL+C");
+		
+		QueueingConsumer consumer = new QueueingConsumer(channel);
+		channel.basicConsume(QUEUE_NAME, true, consumer);
+		
+		while(true) {
+			QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+			String message= new String(delivery.getBody());
+			System.out.println("[x] Received '"+message+"'");
+			
+		}
+		
+	}
+}
